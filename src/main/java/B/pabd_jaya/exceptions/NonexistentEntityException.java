09c2138b18978f60c725bb371ce2c91e793f52e8
AndrePratama11 <andre.ong27@gmail.com>
2022-10-18 20:59:41 +0700

    Add JPA

---
+++ src/main/java/B/pabd_jaya/exceptions/NonexistentEntityException.java
@@ -0,0 +1,10 @@
+package B.pabd_jaya.exceptions;
+
+public class NonexistentEntityException extends Exception {
+    public NonexistentEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public NonexistentEntityException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/B/pabd_jaya/exceptions/PreexistingEntityException.java
@@ -0,0 +1,10 @@
+package B.pabd_jaya.exceptions;
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}

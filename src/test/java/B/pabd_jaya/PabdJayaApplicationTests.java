27c7306210b2231350a5528f10b22ff12f6e500c
AndrePratama11 <andre.ong27@gmail.com>
2022-10-18 20:49:50 +0700

    Input Plugin

---
+++ src/test/java/B/pabd_jaya/PabdJayaApplicationTests.java
@@ -0,0 +1,13 @@
+package B.pabd_jaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PabdJayaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
